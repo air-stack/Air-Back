f1073a4f2bacddfd9b1841bbe1a8f7b932509f70
kevinten10 <596823919@qq.com>
2018-12-10 23:02:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Air Back
+
+> 基于Springboot快速开发后台数据服务，提供REST接口调用，基于Mybatis实现数据的CRUD操作。
+
+## 技术栈
+
+> Springboot + Mybatis + REST
---
+++ src/main/java/com/ten/air/back/AirBackApplication.java
@@ -0,0 +1,12 @@
+package com.ten.air.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirBackApplication.class, args);
+    }
+}
